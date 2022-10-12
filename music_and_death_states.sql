4133c29546c2e200c9ac3beea9bf4cd540a0f022
ivan <oscar.delacruz19f@gmail.com>
2022-10-12 12:21:03 -0500

    add musicians info

--- music_and_death_states.sql
+++ music_and_death_states.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `music_and_death` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `music_and_death`;
 -- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
 --
 -- Host: localhost    Database: music_and_death
@@ -50,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-11 23:10:04
+-- Dump completed on 2022-10-12 12:19:44
