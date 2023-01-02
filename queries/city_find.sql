efc05bd1901771f3054d3ca260c1f13446f66d4a
Kalincillo <oscar.delacruz19f@gmail.com>
2023-01-01 18:14:46 -0600

    add more 50's musicians and queries to find cities and repeated names

---
+++ queries/city_find.sql
@@ -0,0 +1,12 @@
+SELECT 
+    cities.id Id,
+    cities.name City,
+    states.name State,
+    countries.name
+FROM
+    cities
+        JOIN
+    states ON cities.state_id = states.id
+        JOIN
+    countries ON states.country_id = countries.id
+where cities.name = ""
---
+++ queries/name_find.sql
@@ -0,0 +1,6 @@
+SELECT 
+    musician.musician_id, musician.stage_name, stage_lastname
+FROM
+    music_and_death.musician
+WHERE
+    stage_name LIKE 'kansas';
