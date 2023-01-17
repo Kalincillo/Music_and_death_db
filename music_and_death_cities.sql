b5d24f0a18681fbff90b47cd2d6c92f812e4d992
Kalincillo <82189871+Kalincillo@users.noreply.github.com>
2023-01-17 13:51:24 -0600

    add musicians up to 216

--- music_and_death_cities.sql
+++ music_and_death_cities.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: localhost    Database: music_and_death
 -- ------------------------------------------------------
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-16 22:06:22
+-- Dump completed on 2023-01-17 13:50:19
