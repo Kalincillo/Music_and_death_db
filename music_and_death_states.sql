0b2f08bbf13c22d64fb6b96f4c0c2565924c66da
Kalincillo <82189871+Kalincillo@users.noreply.github.com>
2023-02-07 08:25:27 -0600

    add up to 585 musicians

--- music_and_death_states.sql
+++ music_and_death_states.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `music_and_death` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `music_and_death`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: localhost    Database: music_and_death
@@ -48,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-04  9:02:33
+-- Dump completed on 2023-02-07  8:25:01
